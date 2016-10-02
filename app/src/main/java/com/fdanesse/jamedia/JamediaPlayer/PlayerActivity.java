bcc8c360a93d1f0aacb5e4183eb11ee9990bd3d2
flavio <fdanesse@gmail.com>
2016-10-01 22:12:59 -0300

    Reproductor

--- app/src/main/java/com/fdanesse/jamedia/JamediaPlayer/PlayerActivity.java
+++ app/src/main/java/com/fdanesse/jamedia/JamediaPlayer/PlayerActivity.java
@@ -1,17 +1,26 @@
 package com.fdanesse.jamedia.JamediaPlayer;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 
 import com.fdanesse.jamedia.PlayerList.ListActivity;
 import com.fdanesse.jamedia.R;
 
+import java.io.IOException;
+
+//http://www.tutorialspoint.com/android/android_mediaplayer.htm
+
 
 public class PlayerActivity extends AppCompatActivity {
+
+    private final MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +28,11 @@ public class PlayerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_player);
 
         Bundle extras = getIntent().getExtras();
-        Snackbar.make((View) findViewById(R.id.textview),
+        Snackbar.make((View) findViewById(R.id.imagen),
                 "Reproducir: " + extras.getString("name", "") + extras.getString("url", ""),
                 Snackbar.LENGTH_SHORT).show();
+
+        play(extras.getString("url", ""));
     }
 
     public boolean onKeyDown(int keycode, KeyEvent event){
@@ -32,5 +43,42 @@ public class PlayerActivity extends AppCompatActivity {
         }
         //return super.onKeyDown(keycode, event);
         return true;
+    }
+
+    private void play(String url){
+
+        if (mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+        }
+
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        try {
+            mediaPlayer.setDataSource(url);
+        } catch (IllegalArgumentException e1) {
+            e1.printStackTrace();
+        } catch (SecurityException e1) {
+            e1.printStackTrace();
+        } catch (IllegalStateException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+            public void onBufferingUpdate(MediaPlayer mp, int percent) {
+                Log.i("***** Buffering:", "" + percent);
+            }
+        });
+
+        mediaPlayer.prepareAsync();
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mp) {
+                mediaPlayer.start();
+            }
+        });
     }
 }
--- app/src/main/java/com/fdanesse/jamedia/PlayerList/ItemListAdapter.java
+++ app/src/main/java/com/fdanesse/jamedia/PlayerList/ItemListAdapter.java
@@ -1,13 +1,8 @@
 package com.fdanesse.jamedia.PlayerList;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +12,6 @@ import android.widget.TextView;
 import com.fdanesse.jamedia.JamediaPlayer.PlayerActivity;
 import com.fdanesse.jamedia.R;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -26,7 +20,6 @@ import java.util.ArrayList;
 public class ItemListAdapter extends RecyclerView.Adapter<ItemListAdapter.ItemListViewHolder>{
 
     private ArrayList<ListItem> lista;
-    private final MediaPlayer mediaPlayer = new MediaPlayer();
     private Activity activity;
 
     public ItemListAdapter(ArrayList<ListItem> lista, Activity activity){
@@ -45,9 +38,6 @@ public class ItemListAdapter extends RecyclerView.Adapter<ItemListAdapter.ItemLi
             public void onClick(View view) {
                 TextView textview1 = (TextView) view.findViewById(R.id.nombre);
                 TextView textview2 = (TextView) view.findViewById(R.id.url);
-                //Snackbar.make(view, textview.getText(), Snackbar.LENGTH_SHORT).show();
-                //play(textview.getText().toString());
-
                 Intent intent = new Intent(activity, PlayerActivity.class);
                 intent.putExtra("name", textview1.getText().toString());
                 intent.putExtra("url", textview2.getText().toString());
@@ -58,45 +48,6 @@ public class ItemListAdapter extends RecyclerView.Adapter<ItemListAdapter.ItemLi
 
         return new ItemListViewHolder(v);
     }
-
-    /*
-    private void play(String url){
-
-        if (mediaPlayer.isPlaying()){
-            mediaPlayer.stop();
-            mediaPlayer.reset();
-        }
-
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-        try {
-            mediaPlayer.setDataSource(url);
-        } catch (IllegalArgumentException e1) {
-            e1.printStackTrace();
-        } catch (SecurityException e1) {
-            e1.printStackTrace();
-        } catch (IllegalStateException e1) {
-            e1.printStackTrace();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-
-        mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
-            public void onBufferingUpdate(MediaPlayer mp, int percent) {
-                Log.i("Buffering", "" + percent);
-            }
-        });
-
-        mediaPlayer.prepareAsync();
-
-        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-
-            public void onPrepared(MediaPlayer mp) {
-                mediaPlayer.start();
-            }
-        });
-    }
-    */
 
     @Override
     public void onBindViewHolder(ItemListViewHolder holder, int position) {
