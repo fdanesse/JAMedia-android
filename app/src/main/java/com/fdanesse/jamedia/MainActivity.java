c05e43a41b56537d7cd2b5c2820c345f5a7dd3d3
flavio <fdanesse@gmail.com>
2016-10-05 00:48:33 -0300

    detector de conectividad

--- app/src/main/java/com/fdanesse/jamedia/MainActivity.java
+++ app/src/main/java/com/fdanesse/jamedia/MainActivity.java
@@ -1,7 +1,9 @@
 package com.fdanesse.jamedia;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.design.widget.Snackbar;
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private Button radio;
     private Button television;
     private Button archivos;
+
+    private NetworkChangeReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,22 +40,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        boolean i = network_check();
-        radio.setEnabled(i);
-        television.setEnabled(i);
+        network_changed();
 
-        if (i == false){
-            radio.setAlpha(0.5f);
-            television.setAlpha(0.5f);
-            Snackbar.make(radio, "No tienes conexión a internet", Snackbar.LENGTH_INDEFINITE);
-        }
-        else{
-            radio.setAlpha(1.0f);
-            television.setAlpha(1.0f);}
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        receiver = new NetworkChangeReceiver();
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
     }
 
     private boolean network_check(){
-        // https://developer.android.com/training/monitoring-device-state/connectivity-monitoring.html?hl=es
+        /**
+         * https://developer.android.com/training/monitoring-device-state/connectivity-monitoring.html?hl=es
+         */
         ConnectivityManager cm = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         if (activeNetwork != null){
@@ -59,4 +64,37 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
-}
+
+    private void network_changed(){
+        boolean i = network_check();
+        radio.setEnabled(i);
+        television.setEnabled(i);
+
+        if (i == false){
+            radio.setAlpha(0.5f);
+            television.setAlpha(0.5f);
+            Snackbar.make(radio, "No tienes conexión a internet", Snackbar.LENGTH_INDEFINITE).show();
+        }
+        else{
+            radio.setAlpha(1.0f);
+            television.setAlpha(1.0f);}
+    }
+
+    public class NetworkChangeReceiver extends BroadcastReceiver {
+        /**
+         * http://www.mysamplecode.com/2013/04/android-automatically-detect-internet-connection.html
+         */
+
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            //Log.v(LOG_TAG, "Receieved notification about network status");
+            isNetworkAvailable(context);
+        }
+
+        private boolean isNetworkAvailable(Context context) {
+            network_changed();
+            return true;
+        }
+
+    } //NetworkChangeReceiver
+} //MainActivity
