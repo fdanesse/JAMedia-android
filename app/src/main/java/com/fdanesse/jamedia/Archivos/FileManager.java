01192de7d0228604e2ca155e7e5469e1b8447339
flavio <fdanesse@gmail.com>
2017-01-11 12:10:36 -0300

    Correcciones

--- app/src/main/java/com/fdanesse/jamedia/Archivos/FileManager.java
+++ app/src/main/java/com/fdanesse/jamedia/Archivos/FileManager.java
@@ -108,36 +108,42 @@ public class FileManager {
 
     public static ArrayList<ListItem> get_radios(){
         ArrayList<ListItem> lista = new ArrayList<ListItem>();
-        lista.add(new ListItem(R.drawable.jamedia, "Oceano FM 93.9 (Montevideo - Uruguay)", "http://radio4.oceanofm.com:8010/"));
-        lista.add(new ListItem(R.drawable.jamedia, "RauteMusik JaM (Alemania)", "http://main-office.rautemusik.fm"));
-        lista.add(new ListItem(R.drawable.jamedia, "Sawt Beirut International (Líbano)", "http://sawtbeirut.com:8068/"));
-        lista.add(new ListItem(R.drawable.jamedia, "ABCLounge", "http://streaming208.radionomy.com:80/ABC-Lounge"));
-        lista.add(new ListItem(R.drawable.jamedia, "ACZEN'HITS", "http://streaming208.radionomy.com:80/AC-ZEN-HITS"));
-        lista.add(new ListItem(R.drawable.jamedia, "AMLO (Venezuela)", "http://stream.radioamlo.info:8010"));
-        lista.add(new ListItem(R.drawable.jamedia, "ANTENA ZAGREB", "http://173.192.137.34:8050/"));
-        lista.add(new ListItem(R.drawable.jamedia, "CW 41 (San José - Uruguay)", "http://server-uk1.radioseninternetuy.com:8148"));
-        lista.add(new ListItem(R.drawable.jamedia, "AirCDHitsRadio", "http://streaming208.radionomy.com:80/AirCD-Hits-Radio"));
-        lista.add(new ListItem(R.drawable.jamedia, "Amanecer 91.9 FM (Colonia - Uruguay)", "http://67.222.25.168:7028/"));
-        lista.add(new ListItem(R.drawable.jamedia, "ArabMusicRadio", "http://streaming208.radionomy.com:80/ArabMusicRadio"));
-        lista.add(new ListItem(R.drawable.jamedia, "CW 39 La Voz De Paysandú 1320 AM (Paysandú - Uruguay)", "http://todoserver.com:8939/"));
-        lista.add(new ListItem(R.drawable.jamedia, "Continental 1600 AM (Atlántida - Uruguay)", "http://usa15.ciudaddigital.com.uy:8056/ContinentalAM"));
-        lista.add(new ListItem(R.drawable.jamedia, "Corazón on line (Colombia)", "http://radiolatina.info:9456/"));
-        lista.add(new ListItem(R.drawable.jamedia, "Del Hum 89.3 FM (Soriano - Uruguay)", "http://usa15.ciudaddigital.com.uy:8102/DelHumFM"));
-        lista.add(new ListItem(R.drawable.jamedia, "Difusora Soriano 1210 AM (Soriano - Uruguay)", "http://usa15.ciudaddigital.com.uy:8094/DifusoraSoriano"));
-        lista.add(new ListItem(R.drawable.jamedia, "El Libertador 1210 AM (Treinta Y Tres - Uruguay)", "http://s4.viastreaming.net:7200/"));
-        lista.add(new ListItem(R.drawable.jamedia, "Emisora Cultura RAC (Colombia)", "http://univirtual.utp.edu.co:8000/"));
-        lista.add(new ListItem(R.drawable.jamedia, "Espectador 810 AM (Montevideo - Uruguay)", "http://streaming.espectador.com/envivo"));
-        lista.add(new ListItem(R.drawable.jamedia, "FM Principal 107.9 (San José - Uruguay)", "http://s7.myradiostream.com:10764"));
-        lista.add(new ListItem(R.drawable.jamedia, "Maxima 97.7 FM (Paysandú - Uruguay)", "http://todoserver.com:8997/"));
-        lista.add(new ListItem(R.drawable.jamedia, "Nosotros 90.7 FM (Lavalleja - Uruguay)", "http://usa15.ciudaddigital.com.uy:8082/NosotrosFM"));
-        lista.add(new ListItem(R.drawable.jamedia, "One Love Hip Hop Radio (Colombia)", "http://listen.radionomy.com/one-love-hip-hop-radio"));
-        lista.add(new ListItem(R.drawable.jamedia, "Radio Centenario 1250 AM (Montevideo - Uruguay)", "http://rfm.radio.netgate.com.uy:8000/centenario"));
-        lista.add(new ListItem(R.drawable.jamedia, "Radio City 95.1 FM (Durazno - Uruguay)", "http://usa15.ciudaddigital.com.uy:8030/CityFM"));
-        lista.add(new ListItem(R.drawable.jamedia, "Radio Prado Sur (Montevideo - Uruguay)", "http://eu1.fastcast4u.com:3552/stream"));
-        lista.add(new ListItem(R.drawable.jamedia, "Radio Rivera 1440 AM (Rivera - Uruguay)", "http://usa15.ciudaddigital.com.uy:8060/RiveraAM"));
-        lista.add(new ListItem(R.drawable.jamedia, "Toros 91.9 FM (Paso De Los Toros - Tacuarembo - Uruguay)", "http://pasodelostoros.com:8008/"));
-        lista.add(new ListItem(R.drawable.jamedia, "Universal 970 AM (Montevideo - Uruguay)", "http://panel.zwcast.com:9008/"));
-        lista.add(new ListItem(R.drawable.jamedia, "Universidad de Bogotá (Colombia)", "http://hjut.utadeo.edu.co:8000"));
+        lista.add(new ListItem(R.drawable.uruguay, "CW 41 (San José)", "http://server-uk1.radioseninternetuy.com:8148"));
+        lista.add(new ListItem(R.drawable.uruguay, "FM Principal 107.9 (San José)", "http://s7.myradiostream.com:10764"));
+        lista.add(new ListItem(R.drawable.uruguay, "Nosotros 90.7 FM (Lavalleja)", "http://usa15.ciudaddigital.com.uy:8082/NosotrosFM"));
+        lista.add(new ListItem(R.drawable.uruguay, "Toros 91.9 FM (Paso De Los Toros)", "http://pasodelostoros.com:8008/"));
+        lista.add(new ListItem(R.drawable.uruguay, "Amanecer 91.9 FM (Colonia)", "http://67.222.25.168:7028/"));
+        lista.add(new ListItem(R.drawable.uruguay, "CW 39 La Voz De Paysandú 1320 AM (Paysandú)", "http://todoserver.com:8939/"));
+        lista.add(new ListItem(R.drawable.uruguay, "Maxima 97.7 FM (Paysandú)", "http://todoserver.com:8997/"));
+        lista.add(new ListItem(R.drawable.uruguay, "El Libertador 1210 AM (Treinta Y Tres)", "http://s4.viastreaming.net:7200/"));
+        lista.add(new ListItem(R.drawable.uruguay, "Radio City 95.1 FM (Durazno)", "http://usa15.ciudaddigital.com.uy:8030/CityFM"));
+        lista.add(new ListItem(R.drawable.uruguay, "Radio Rivera 1440 AM (Rivera)", "http://usa15.ciudaddigital.com.uy:8060/RiveraAM"));
+        lista.add(new ListItem(R.drawable.uruguay, "Continental 1600 AM (Atlántida)", "http://usa15.ciudaddigital.com.uy:8056/ContinentalAM"));
+        lista.add(new ListItem(R.drawable.uruguay, "Del Hum 89.3 FM (Soriano)", "http://usa15.ciudaddigital.com.uy:8102/DelHumFM"));
+        lista.add(new ListItem(R.drawable.uruguay, "Difusora Soriano 1210 AM (Soriano)", "http://usa15.ciudaddigital.com.uy:8094/DifusoraSoriano"));
+
+        lista.add(new ListItem(R.drawable.uruguay, "Oceano FM 93.9 (Montevideo)", "http://radio4.oceanofm.com:8010/"));
+        lista.add(new ListItem(R.drawable.uruguay, "Espectador 810 AM (Montevideo)", "http://streaming.espectador.com/envivo"));
+        lista.add(new ListItem(R.drawable.uruguay, "Radio Centenario 1250 AM (Montevideo)", "http://rfm.radio.netgate.com.uy:8000/centenario"));
+        lista.add(new ListItem(R.drawable.uruguay, "Radio Prado Sur (Montevideo)", "http://eu1.fastcast4u.com:3552/stream"));
+        lista.add(new ListItem(R.drawable.uruguay, "Universal 970 AM (Montevideo)", "http://panel.zwcast.com:9008/"));
+
+        lista.add(new ListItem(R.drawable.colombia, "Corazón on line (Colombia)", "http://radiolatina.info:9456/"));
+        lista.add(new ListItem(R.drawable.colombia, "One Love Hip Hop Radio (Colombia)", "http://listen.radionomy.com/one-love-hip-hop-radio"));
+        lista.add(new ListItem(R.drawable.colombia, "Universidad de Bogotá (Colombia)", "http://hjut.utadeo.edu.co:8000"));
+        lista.add(new ListItem(R.drawable.colombia, "Emisora Cultura RAC (Colombia)", "http://univirtual.utp.edu.co:8000/"));
+
+        lista.add(new ListItem(R.drawable.venezuela, "AMLO (Venezuela)", "http://stream.radioamlo.info:8010"));
+
+        lista.add(new ListItem(R.drawable.reino_unido, "Capital TV (Reino Unido)", "http://media-ice.musicradio.com/CapitalUKMP3"));
+        lista.add(new ListItem(R.drawable.alemania, "RauteMusik JaM (Alemania)", "http://main-office.rautemusik.fm"));
+        lista.add(new ListItem(R.drawable.libano, "Sawt Beirut International (Líbano)", "http://sawtbeirut.com:8068/"));
+        lista.add(new ListItem(R.drawable.francia, "ABC Lounge (Francia)", "http://streaming208.radionomy.com:80/ABC-Lounge"));
+        lista.add(new ListItem(R.drawable.francia, "AC ZEN HITS (Francia)", "http://streaming208.radionomy.com:80/AC-ZEN-HITS"));
+        lista.add(new ListItem(R.drawable.francia, "AirCD Hits Radio (Francia)", "http://streaming208.radionomy.com:80/AirCD-Hits-Radio"));
+        lista.add(new ListItem(R.drawable.francia, "Arab Music Radio (Francia)", "http://streaming208.radionomy.com:80/ArabMusicRadio"));
+        lista.add(new ListItem(R.drawable.croacia, "Antena Zagreb (Croacia)", "http://173.192.137.34:8050/"));
+
         return lista;
     }
 
